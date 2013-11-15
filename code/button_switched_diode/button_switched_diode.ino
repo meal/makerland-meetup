83b3fc1922584676550a8f2986858f23583f9642
Mateusz Kozak <mateusz@mkozak.pl>
2013-11-15 22:41:22 +0100

    code fix

--- code/button_switched_diode/button_switched_diode.ino
+++ code/button_switched_diode/button_switched_diode.ino
@@ -1,17 +1,20 @@
+
 // constants won't change. They're used here to 
 // set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  10;      // the number of the LED pin
+const int buttonPin = A0;     // the number of the button pin
+const int ledPin =  A5;      // the number of the LED pin
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);      
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);     
-
+  pinMode(ledPin, OUTPUT);
+  // initialize the button pin as an input:
+  pinMode(buttonPin, INPUT);
+  // set button pin to HIGH to enable built-in pull-up (so there is 
+  // no need to use resistor between button and arduino)
+  digitalWrite(buttonPin, HIGH);
 }
 
 void loop(){
@@ -20,7 +23,7 @@ void loop(){
 
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {     
+  if (buttonState != HIGH) {     
     // turn LED on:    
     digitalWrite(ledPin, HIGH);  
   } 
